cada1827532da5e908ca669f1b016524b3a28a0e
Lucas Souza <souza.lucas01@gmail.com>
2020-03-08 14:04:27 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -9,3 +9,11 @@ def display_board(board)
   puts "-----------"
   puts "   |   |   "
 end
+
+def display_board(board)
+  puts "   |   |   "
+  puts "-----------"
+  puts "   | #{board[4]}  |   "
+  puts "-----------"
+  puts "   |   |   "
+end
